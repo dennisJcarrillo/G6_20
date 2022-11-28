17c37c46b62c56e691fd5ef8f762e8e9b85d2186
MeSalgado0826 <mosalgado@unah.hn>
2022-11-27 18:40:49 -0600

    Subido cambio del método Get a Post

--- routes/router.js
+++ routes/router.js
@@ -9,21 +9,21 @@ var  ReservaController = require("../controllers/reserva-controller"),
 router
   //****ALUMNO EJEMPLO****
   .get("/vuelo/getall", VueloController.getAll)
-  .get("/vuelo/getone/:codigo_vuelo", VueloController.getOne)
+  .post("/vuelo/getone/:codigo_vuelo", VueloController.getOne)
   .post("/vuelo/insertar/:codigo_vuelo", VueloController.post)
   .put("/vuelo/actualizar/:codigo_vuelo", VueloController.put)
   .delete("/vuelo/eliminar/:codigo_vuelo", VueloController.delete)
 
   //****RUTAS ENTIDAD PASAJERO****
   .get("/pasajero/getAll", PasajeroController.getAll)
-  .get("/pasajero/getOne/:codigo_pasajero", PasajeroController.getOne)
+  .post("/pasajero/getOne/:codigo_pasajero", PasajeroController.getOne)
   .post("/pasajero/insertar/:codigo_pasajero", PasajeroController.post)
   .put("/pasajero/actualizar/:codigo_pasajero", PasajeroController.put)
   .delete("/pasajero/eliminar/:codigo_pasajero", PasajeroController.delete)
 
    //****RUTAS ENTIDAD RESERVA****
   .get("/reserva/getall", ReservaController.getAll)
-  .get("/reserva/getone/:numero_reservacion", ReservaController.getOne)
+  .post("/reserva/getone/:numero_reservacion", ReservaController.getOne)
   .post("/reserva/insertar/:numero_reservacion", ReservaController.post)
   .put("/reserva/actualizar/:numero_reservacion", ReservaController.put)
   .delete("/reserva/eliminar/:numero_reservacion", ReservaController.delete)
